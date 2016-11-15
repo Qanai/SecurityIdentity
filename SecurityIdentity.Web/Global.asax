ed161f0025ad5e4f0ed5f330b0d362d9f019a90d
Eliyahu Shlieserman <eliyahushli@gmail.com>
2016-11-15 19:14:42 +0200

    set

---
+++ SecurityIdentity.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SecurityIdentity.Web.MvcApplication" Language="C#" %>
---
+++ SecurityIdentity.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityIdentity.Web.Startup))]
+namespace SecurityIdentity.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
